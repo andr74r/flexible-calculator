8aa0b5b568ffe76c028c494906b5f35ac6a2ba97
andr74r <andrey.zvyagintsev.1993@gmail.com>
2021-11-06 18:41:55 +0500

    Added division operation

--- src/FlexibleCalculator/FlexibleCalculator.Tests/RpnCalculatorTests.cs
+++ src/FlexibleCalculator/FlexibleCalculator.Tests/RpnCalculatorTests.cs
@@ -10,10 +10,12 @@ namespace FlexibleCalculator.Tests
         [TestCase("2+3-4*3", ExpectedResult = -7)]
         [TestCase("(2+3-4)*3", ExpectedResult = 3)]
         [TestCase("22.311+3.1", ExpectedResult = 25.411)]
+        [TestCase("(2+3)/5", ExpectedResult = 1)]
         public double Calculate_Expression_ShouldReturnCalculatedExrepssion(string input)
         {
             var tokenParser = new TokenParser();
-            var rpnAlgo = new ReversePolishNotationAlgo(new IOperation[] { new PlusOperation(), new MinusOperation(), new MultiplicationOperation() });
+            var rpnAlgo = new ReversePolishNotationAlgo(
+                new IOperation[] { new PlusOperation(), new MinusOperation(), new MultiplicationOperation(), new DivisionOperation() });
             var rpnCalculator = new RpnCalculator(rpnAlgo, tokenParser);
 
             var result = rpnCalculator.Calculate(input);
---
+++ src/FlexibleCalculator/FlexibleCalculator/Operations/DivisionOperation.cs
@@ -0,0 +1,20 @@
+﻿namespace FlexibleCalculator.Operations
+{
+    public class DivisionOperation : IOperation
+    {
+        public double Execute(double x, double y)
+        {
+            return x / y;
+        }
+
+        public string GetOperation()
+        {
+            return "/";
+        }
+
+        public Priority GetPriority()
+        {
+            return Priority.Medium;
+        }
+    }
+}
